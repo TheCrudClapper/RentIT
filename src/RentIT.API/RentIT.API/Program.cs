2fcc7caa1a99015545834ac835bb1249c7debadc
VortexOoN <wojciechmucha12@gmail.com>
2025-07-29 18:11:57 +0200

    Add project files.

---
+++ src/RentIT.API/RentIT.API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Enables swagger to read the endpoints of application
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( options =>
+{
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "RentItApi.xml"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseHsts();
+app.UseHttpsRedirection();
+
+//Enables endpoint for swagger.json file (openAPI specification)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
