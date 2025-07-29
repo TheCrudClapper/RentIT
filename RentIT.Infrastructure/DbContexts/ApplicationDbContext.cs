452d8efba1af6650c88c7a358a7f13c6bc892f16
VortexOoN <wojciechmucha12@gmail.com>
2025-07-29 20:41:06 +0200

    feat: add and cofigure EF Core for MSSQL Database
    Created entities, DbContext,

---
+++ RentIT.Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentIT.Core.Domain.Entities;
+
+namespace RentIT.Infrastructure.DbContexts
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Equipment> EquipmentItems { get; set; }
+        public virtual DbSet<Rental> Rentals { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions options) :base(options)
+        {
+        }
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Equipment>().Property(prop => prop.Status)
+                .HasConversion<string>();
+        }
+
+    }
+}
--- src/RentIT.API/RentIT.API/Program.cs
+++ src/RentIT.API/RentIT.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using RentIT.Infrastructure.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
+        x => x.MigrationsAssembly("RentIT.Infrastructure"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
