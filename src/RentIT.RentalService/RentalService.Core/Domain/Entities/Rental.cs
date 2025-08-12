7e2f8da2319dfaeec9a6975f28aef32e84e55bce
VortexOoN <wojciechmucha12@gmail.com>
2025-08-12 21:14:53 +0200

    feat: organize project into microservice architecture

---
+++ src/RentIT.RentalService/RentalService.Core/Domain/Entities/Rental.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentIT.Core.Domain.Entities
+{
+    /// <summary>
+    /// Represents a rental entity that contains information about the equipment associated with the rental.
+    /// </summary>
+    /// <remarks>The <see cref="Rental"/> class is used to manage and track equipment rentals. It includes a
+    /// collection of  <see cref="Equipment"/> objects that are part of the rental. This class inherits from <see
+    /// cref="BaseEntity"/>,  which provides common properties for all entities in the system.</remarks>
+    public class Rental : BaseEntity
+    {
+        public Guid EquipmentId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal RentalPrice { get; set; }
+        [ForeignKey("EquipmentId")]
+        public Equipment Equipment { get; set; } = null!;
+
+        [ForeignKey("UserId")]
+        public User CreatedBy { get; set; } = null!;
+
+    }
+}
