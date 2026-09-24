85efba07ca4ff1484d2a49624d5282267f9039b9
TheCrudClapper <wojciechmucha12@gmail.com>
2026-09-24 20:38:21 +0200

    feat: perform database migration

---
+++ src/RentIT.EquipmentService/EquipmentService.Infrastructure/Migrations/20260924183455_RentalListingMigration.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RentalListingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EquipmentItems");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.CreateTable(
+                name: "RentIt.Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentIt.Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentIt.Equipments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    InternalNotes = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Condition = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentIt.Equipments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RentIt.Equipments_RentIt.Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "RentIt.Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentImage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourcePath = table.Column<string>(type: "text", nullable: false),
+                    EquipmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EquipmentImage_RentIt.Equipments_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "RentIt.Equipments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentIt.RentalListings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EquipmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    PricePerDay_CurrencyCode = table.Column<string>(type: "text", nullable: false),
+                    PricePerDay_Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    PenaltyFeePerDay_CurrencyCode = table.Column<string>(type: "text", nullable: false),
+                    PenaltyFeePerDay_Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    ListingStatus = table.Column<string>(type: "text", nullable: false),
+                    MaximumRentalDays = table.Column<int>(type: "integer", nullable: false),
+                    MinimumRentalDays = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentIt.RentalListings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RentIt.RentalListings_RentIt.Equipments_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "RentIt.Equipments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListingImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourcePath = table.Column<string>(type: "text", nullable: false),
+                    ListingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RentalListingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListingImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ListingImages_RentIt.RentalListings_RentalListingId",
+                        column: x => x.RentalListingId,
+                        principalTable: "RentIt.RentalListings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentImage_EquipmentId",
+                table: "EquipmentImage",
+                column: "EquipmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListingImages_RentalListingId",
+                table: "ListingImages",
+                column: "RentalListingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentIt.Equipments_CategoryId",
+                table: "RentIt.Equipments",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentIt.RentalListings_EquipmentId",
+                table: "RentIt.RentalListings",
+                column: "EquipmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EquipmentImage");
+
+            migrationBuilder.DropTable(
+                name: "ListingImages");
+
+            migrationBuilder.DropTable(
+                name: "RentIt.RentalListings");
+
+            migrationBuilder.DropTable(
+                name: "RentIt.Equipments");
+
+            migrationBuilder.DropTable(
+                name: "RentIt.Categories");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AverageRating = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    CreatedByUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateDeleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DateEdited = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Notes = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    RentalPricePerDay = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    ReviewCount = table.Column<int>(type: "integer", nullable: false),
+                    SerialNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EquipmentItems_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentItems_CategoryId",
+                table: "EquipmentItems",
+                column: "CategoryId");
+        }
+    }
+}
